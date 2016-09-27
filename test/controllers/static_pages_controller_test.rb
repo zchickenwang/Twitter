c5dd9af471cbc5fe23b4d3acd0e0f6a144a2ea3c
Z Wang <zzwang@berkeley.edu>
2016-09-27 12:32:44 -0700

    links

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Twitter"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
+  #test "should get root" do
+  #  get root_path
+  #  assert_response :success
+  #end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "#{@base_title} Help"
   end
 
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title," "#{@base_title} Contact"
+  end
+
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "#{@base_title} About"
   end
