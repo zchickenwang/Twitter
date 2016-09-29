6d9fdb11de3e4c9c0d312d3a376323894ea60624
Z Wang <zzwang@berkeley.edu>
2016-09-29 12:04:02 -0700

    advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,32 @@ class User < ApplicationRecord
     before_save { email.downcase! }
     has_secure_password
 
+    attr_accessor :remember_token
+
     # Returns the hash digest of the given string.
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Forgets a user.
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
+
+    # Returns true if the given token matches the digest.
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
     end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,6 +21,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
@@ -35,6 +37,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 
 
