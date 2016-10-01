4a8a6859ef6781c221ec9353b17f4e2604261a12
Z Wang <zzwang@berkeley.edu>
2016-09-30 19:02:51 -0700

    pass reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   get '/about',     to: 'static_pages#about'
@@ -15,4 +19,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
