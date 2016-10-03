f1c3423815b8fd9d78f68691549e866ffe49e0ee
Z Wang <zzwang@berkeley.edu>
2016-10-02 19:36:11 -0700

    user followering

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
-  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
 
   get '/about',     to: 'static_pages#about'
   get '/help',      to: 'static_pages#help'
@@ -14,11 +10,21 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_pages#home'
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
 
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+  
 end
